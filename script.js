d21da3284590a9bc04b89ac59eea1da2132f0d39
Diegotyb <98501751+Diegotyb@users.noreply.github.com>
2024-02-21 23:08:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// app.js
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAMIG0OtcBfNeuLV03h5AdgPktGgUsI71I",
+    authDomain: "profile-app-a4d61.firebaseapp.com",
+    databaseURL: "https://profile-app-a4d61-default-rtdb.firebaseio.com",
+    projectId: "profile-app-a4d61",
+    storageBucket: "profile-app-a4d61.appspot.com",
+    messagingSenderId: "895876226030",
+    appId: "1:895876226030:web:6881693b3dac7fc7834cff",
+    measurementId: "G-XW8LMTE2PX"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
+
+
+
+
+
+
+
+
